28f21697a75e04c2f506964f8a3bd0d1e4ed81a4
Aryan Shukla <as562734880@gmail.com>
2026-09-12 16:03:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int reverseNumber(int n, long rev) {
+
+        if(n == 0)
+            return rev;
+
+        int digit = n % 10;
+
+        rev = rev * 10 + digit;
+
+        return reverseNumber(n / 10, rev);
+    }
+
+    bool isPalindrome(int x) {
+
+        if(x < 0)
+            return false;
+
+        int original = x;
+
+        int reversed = reverseNumber(x, 0);
+
+        return original == reversed;
+    }
+};
