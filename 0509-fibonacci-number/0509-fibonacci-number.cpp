15557b7452d9f4e7d67522c1eed217ca89510874
Aryan Shukla <as562734880@gmail.com>
2026-09-19 17:05:23 +0530

    Time: 14 ms (22.48%), Space: 7.8 MB (37.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
+        //Base case 
         if(n <= 1)
         return n;
 
+        // yaha pe number tabtak recursively solve hoga jabtak vo base case ko satisfy na karde 
         int last  = fib(n-1);
         int slast = fib(n-2);
         return last + slast;
