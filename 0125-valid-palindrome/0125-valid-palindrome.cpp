fdc6f4d5ac8040e8e67f466ebe9640d7fa74b2b1
Aryan Shukla <as562734880@gmail.com>
2026-09-19 16:53:22 +0530

    Time: 3 ms (27.72%), Space: 10 MB (34.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
-class Solution{
+class Solution {
 public:
-bool isPalindrome(int left, int right, string& s) {
+    bool Palindrome(int i, int j, string& s) {
 
-    if(left <= right && !isalnum(s[left]))
-        return isPalindrome(left + 1, right, s);
-
-    if(left <= right && !isalnum(s[right]))
-        return isPalindrome(left, right - 1, s);
-
-    if(left >= right)
+        // yeh part hai alphanumeric vala
+        //yaha pe humne Notaplhanumeric vali condition li hai.
+        if(i < s.size()&& !isalnum(s[i])){
+            return Palindrome(i + 1, j, s);
+        }
+        if(j < s.size() && !isalnum(s[j])){
+            return Palindrome(i, j - 1, s);
+        }
+        //Yaha se base case agya, base case pehle yaa baad mai kabhi bhi laga do based on logic
+        if(i >= j)
         return true;
 
-    if(tolower(s[left]) != tolower(s[right]))
+        //main case compare kardiya
+        if(tolower(s[i]) != tolower(s[j]))
         return false;
 
-    return isPalindrome(left + 1, right - 1, s);
-}
-    bool isPalindrome(string s){
-    return isPalindrome(0, s.size() - 1, s);
+        return Palindrome(i + 1, j - 1, s);
+    }
+
+    bool isPalindrome(string x){
+        string s = x;
+        return Palindrome(0, s.size() - 1, s); 
     }
 };
